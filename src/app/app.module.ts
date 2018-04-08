225e091d0ffd20964e859ae60327c7ee61cb1fb5
TodayCode <onetoday1030@gmail.com>
2018-04-08 18:42:50 -0500

    samples added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { ForMaterialModule } from "./for-material.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ForMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
